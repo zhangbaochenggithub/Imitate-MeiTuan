a848be54c9ccc1a84ecbad67adb7a25f418f0100
unknown <unknown@example.com>
2018-11-19 10:41:19 +0800

    Initial Commit

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,119 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    urlArry:[{id:'pink', num:'01'},{id:'orange', num:'02'},{id:'skyblue', num:'03'}],
+    entranceArry: [{ id: '美食', num: '01' }, { id: '猫眼电影', num: '02' }, { id: '酒店住宿', num: '03' }, { id: '休闲娱乐', num: '04' }, { id: '外卖', num: '05' }],
+    entranceArry_2: [{ id: 'KTV', num: '06' }, { id: '丽人/美发', num: '07' }, { id: '景点门票', num: '08' }, { id: '火车票', num: '09' }, { id: '民宿/公寓', num: '10' }],
+    grouponArry: [{ id: 'one', num: '01' }, { id: 'two', num: '02' }, { id: 'three', num: '03' }, { id: 'four', num: '04' }, { id: 'five', num: '05' }, { id: 'six', num: '06' },{ id: 'seven', num: '07' }]
+  },
+
+  onLoad: function() {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  onGetUserInfo: function(e) {
+    if (!this.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
+  },
+
+  onGetOpenid: function() {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        wx.navigateTo({
+          url: '../userConsole/userConsole',
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
+  },
+
+  // 上传图片
+  doUpload: function () {
+    // 选择图片
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+
+        wx.showLoading({
+          title: '上传中',
+        })
+
+        const filePath = res.tempFilePaths[0]
+        
+        // 上传图片
+        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
+        wx.cloud.uploadFile({
+          cloudPath,
+          filePath,
+          success: res => {
+            console.log('[上传文件] 成功：', res)
+
+            app.globalData.fileID = res.fileID
+            app.globalData.cloudPath = cloudPath
+            app.globalData.imagePath = filePath
+            
+            wx.navigateTo({
+              url: '../storageConsole/storageConsole'
+            })
+          },
+          fail: e => {
+            console.error('[上传文件] 失败：', e)
+            wx.showToast({
+              icon: 'none',
+              title: '上传失败',
+            })
+          },
+          complete: () => {
+            wx.hideLoading()
+          }
+        })
+
+      },
+      fail: e => {
+        console.error(e)
+      }
+    })
+  },
+
+})
